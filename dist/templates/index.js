ff1a442edbc0c28af702f51dd9b439ee6b4932e9
ErykZenith <167299127+ErykZenith@users.noreply.github.com>
2025-01-15 16:44:59 +0700

    Update index.js

--- dist/templates/index.js
+++ dist/templates/index.js
@@ -1,6 +1,20 @@
+const sanitize = (str) => {
+    return String(str)
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#39;")
+        .replace(/\//g, "&#47;")
+        .replace(/\(/g, "&#40;")
+        .replace(/\)/g, "&#41;")
+        .replace(/\+/g, "&#43;")
+        .replace(/=/g, "&#61;");
+};
+
 export const TEMPLATES = {
-    error: (e) => `<div class="error">${e.msg}</div>`,
-    info: (e) => `<div class="info">${e.msg}</div>`,
+    error: (e) => `<div class="error">${sanitize(e.msg)}</div>`,
+    info: (e) => `<div class="info">${sanitize(e.msg)}</div>`,
 }
 
 export default TEMPLATES
